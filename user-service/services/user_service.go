899998d2fb6f8746487371c936feb553ce23e9de
Sundaram Kumar Jha <jhasundaram@outlook.com>
2024-12-21 17:57:06 +0530

    Update user_service.go

--- user-service/services/user_service.go
+++ user-service/services/user_service.go
@@ -1,4 +1,3 @@
-// services/user_service.go
 package services
 
 import (
@@ -12,20 +11,16 @@ type UserService struct {
 	DB *gorm.DB
 }
 
-// NewUserService creates a new UserService instance
 func NewUserService(db *gorm.DB) *UserService {
 	return &UserService{DB: db}
 }
 
-// RegisterUser registers a new user in the database
 func (s *UserService) RegisterUser(user *models.User) error {
-	// Check if the user already exists
 	var existingUser models.User
 	if err := s.DB.Where("username = ?", user.Username).First(&existingUser).Error; err == nil {
 		return errors.New("user already exists")
 	}
 
-	// Create a new user
 	if err := s.DB.Create(user).Error; err != nil {
 		return err
 	}
@@ -33,7 +28,6 @@ func (s *UserService) RegisterUser(user *models.User) error {
 	return nil
 }
 
-// AuthenticateUser verifies the user's credentials and returns a boolean status
 func (s *UserService) AuthenticateUser(username, password string) (*models.User, error) {
 	var user models.User
 	if err := s.DB.Where("username = ? AND password = ?", username, password).First(&user).Error; err != nil {
